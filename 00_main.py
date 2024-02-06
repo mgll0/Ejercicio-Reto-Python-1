36905f3fc550087ece06de9a410347d4daa46bf3
Miguel Garcia <92490925+mgll0@users.noreply.github.com>
2024-02-05 19:54:03 -0600

    Add files via upload

---
+++ 00_main.py
@@ -0,0 +1,7 @@
+print("Saludos, introduce la siguiente informacion:")
+nombre = input("Nombre(s): ")
+apellido = input("Apellidos: ")
+telefono = input("Telefono: ")
+correo = input("Correo Electronico: ")
+
+print("Hola " + nombre + " " + apellido + ", en breve recibiras un correo a " + correo)
